6dbb7162215fb7d7d24c5c30806ed7627e853b22
Vladi <vladimir.ciuculescu@gmail.com>
2024-08-07 21:05:17 +0300

    Enabled transition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,9 +31,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          // defaultTheme="dark"
           enableSystem
-          disableTransitionOnChange
         >
           <NavBar />
 
